ee993e817e70595846072efaf5be0c4b2db4c6fb
Osada Chamikara <osadabro2@gmail.com>
2024-08-31 21:41:04 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -10,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please Give Me a Url Or song Name")
+if(!q) return reply("*Please Give Me a Url Or song Name ‼️*")
 const search = await yts(q)
 const data = search.videos[0];
 const url = data.url
@@ -38,6 +38,56 @@ let downloadUrl = down.dl_url
 
 //Send Audio 
 await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+//await conn.sendMessage(from,{documnet:{url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title+ ".mp4",caption:"made by Osada-md"},{quoted:mek})
+
+    
+}catch(e){
+console.log(e)
+reply("${e}")
+}
+})
+
+
+//=============Video==============
+
+cmd({
+    pattern: "video",
+    desc: "Download video",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please Give Me a Url Or Video Name ‼️*")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc =`
+
+*📁Video DOWNLOADER ⬇️*
+
+*🏷️ Title  -* ${data.title}
+*🕐 Time   -* ${data.timestamp}
+*🏷️ Ago    -* ${data.ago}
+*🧑‍🧑‍🧒‍🧒 Views -* ${data.views}
+
+```🎧Downloading Your Video Please wait...```
+
+🔮ᴘᴏᴡᴇʀᴅ ʙʏ *ᴏꜱᴀᴅᴀ-ᴍᴅ*
+
+`
+
+await conn.sendMessage(from,{image:{url:data.thumbnail},caption:desc},{quoted:mek});
+
+//Download Auodio 
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url 
+
+//Send Audio 
+await conn.sendMessage(from,{video:{url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+//await conn.sendMessage(from,{documnet:{url:downloadUrl},mimetype:"video/mp4",fileName:data.title+ ".mp4"},{quoted:mek})
+
 
 }catch(e){
 console.log(e)
