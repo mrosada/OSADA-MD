043b3994c67d9b404cee4fef912be1535400fa93
Osada Chamikara <osadabro2@gmail.com>
2024-08-31 19:38:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG, || "",
+SESSION_ID: process.env.SESSION_ID, || "x70WVY6I#mWAdIg0bvYAQeumHkMk0755eQNLX5MnafrrC-wXQsL8",
+ALIVE_IMG: process.env.ALIVE_IMG, || "https://i.ibb.co/nsNpzrh/Blue-and-White-Modern-The-Future-Of-Artificial-Intelligence-You-Tube-Thumbnail.png",
 ALIVE_MSG: process.env.ALIVE_MSG, || "Hello, I am Alive",
 };
