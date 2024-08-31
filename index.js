94f2e46cfda1afa4d9bbbededd83fa3db6d6c01c
Osada Chamikara <osadabro2@gmail.com>
2024-08-31 19:33:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94779415698']
+const ownerNumber = ['447710173736']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
@@ -69,9 +69,13 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-let up = `Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
+let up = `*OSADA MD CONNECTED ✅*\n\n
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+📎 *Github :* https://github.com/mrosada/OSADA-MD\n\n
+
+📎 *Web :* https://mr-osada.vercel.app/`;
+
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.ibb.co/TLYY1DS/20240831-192907.jpg` }, caption: up })
 
 }
 })
