f188e2b378c914bb4dfd048cdbdf10e1e63786f6
Osada Chamikara <osadabro2@gmail.com>
2024-09-01 08:58:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,13 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
-//============================================================================ 
+//=======================WorkType===================================================== 
+
+if(!isOwner && config.MODE ==="private") return
+if(!isOwner && isGroup && config.MODE ==="inbox") return
+if(!isOwner && !isGroup && config.MODE ==="groups") return
+
+//=======================================================================
 
 })
 }
