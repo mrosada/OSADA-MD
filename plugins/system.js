f25d123894de9012d169047aa5415466cbea8e7a
Osada Chamikara <osadabro2@gmail.com>
2024-09-01 08:44:30 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,28 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias:["status","info"],
+    desc: "Check up time and ram.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = ` *OSADA-MD SYSTEM🛠️* 
+
+*🖥️Uptime:*  ${runtime(process.uptime())}
+*🎚️Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🚹HostName:* ${os.hostname()}
+*👨‍💻Owner:* Osada Chamikara
+`
+return reply(`${status}`)
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
